fc51829645843e6b3be744c33a962d7561a049c8
suryamageshk <83328984+suryamageshk@users.noreply.github.com>
2023-11-10 20:26:33 +0530

    Create DDL_DML.sql

---
+++ DDL_DML.sql
@@ -0,0 +1,58 @@
+-- Table 1: Menu
+CREATE TABLE menu (
+  product_id INTEGER,
+  product_name VARCHAR(5),
+  price INTEGER,
+  PRIMARY KEY (product_id)
+);
+
+INSERT INTO menu
+  (product_id, product_name, price)
+VALUES
+  ('1', 'Sushi', '10'),
+  ('2', 'Curry', '15'),
+  ('3', 'Ramen', '12');
+  
+-- Table 2: Members
+CREATE TABLE members (
+  customer_id VARCHAR(1),
+  join_date DATE,
+  PRIMARY KEY (customer_id)
+);
+
+INSERT INTO members
+  (customer_id, join_date)
+VALUES
+  ('A', '2023-01-07'),
+  ('B', '2023-01-09'),
+  ('C', '2023-01-11');
+
+-- Table 3: Sales
+CREATE TABLE sales (
+  customer_id VARCHAR(1),
+  order_date DATE,
+  product_id INTEGER,
+  FOREIGN KEY (customer_id) REFERENCES members(customer_id),
+  FOREIGN KEY (product_id) REFERENCES menu(product_id)
+);
+
+INSERT INTO sales
+  (customer_id, order_date, product_id)
+VALUES
+  ('A', '2021-01-01', '1'),
+  ('A', '2021-01-01', '2'),
+  ('A', '2021-01-07', '2'),
+  ('A', '2021-01-10', '3'),
+  ('A', '2021-01-11', '3'),
+  ('A', '2021-01-11', '3'),
+  ('B', '2021-01-01', '2'),
+  ('B', '2021-01-02', '2'),
+  ('B', '2021-01-04', '1'),
+  ('B', '2021-01-11', '1'),
+  ('B', '2021-01-16', '3'),
+  ('B', '2021-02-01', '3'),
+  ('C', '2021-01-01', '3'),
+  ('C', '2021-01-01', '3'),
+  ('C', '2021-01-07', '3');
+  
+  
