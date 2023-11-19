1a25db5aa5ecfcb2c7d69eb30a422cb65155a8b0
suryamageshk <83328984+suryamageshk@users.noreply.github.com>
2023-11-19 18:00:13 +0530

    Update Case Study #1 - Danny's Diner.sql

--- Case Study #1 - Danny's Diner.sql
+++ Case Study #1 - Danny's Diner.sql
@@ -132,7 +132,7 @@ ORDER BY
 -- 9. If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
 SELECT 
   customer_id, 
-  SUM(
+  SUM (
     CASE WHEN product_name = 'sushi' THEN price * 10 * 2 ELSE price * 10 END
   ) AS points 
 FROM 
@@ -144,7 +144,7 @@ GROUP BY
 -- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
 SELECT 
   S.customer_id, 
-  SUM(CASE 
+  SUM (CASE 
 		WHEN order_date BETWEEN join_date AND ADDDATE(join_date,INTERVAL 6 day) THEN 
 			price * 10 * 2 
 		WHEN product_name = 'sushi' THEN 
